7446e1653d8ddfc41bb5fc31df85311a786f2f67
dengus <dengus7700@gmail.com>
2022-10-02 17:39:49 +0900

    ok_folder add

---
+++ Rust/ch2/ch2.1/ok_folder/target/debug/deps/ok_folder-8d27cd7a90568eaf.d
@@ -0,0 +1,5 @@
+/Users/ohdoo-il/Desktop/Rust/ch2/ch2.1/ok_folder/target/debug/deps/ok_folder-8d27cd7a90568eaf: ok_folder.rs
+
+/Users/ohdoo-il/Desktop/Rust/ch2/ch2.1/ok_folder/target/debug/deps/ok_folder-8d27cd7a90568eaf.d: ok_folder.rs
+
+ok_folder.rs:
---
+++ Rust/ch2/ch2.1/ok_folder/target/debug/ok_folder.d
@@ -0,0 +1,1 @@
+/Users/ohdoo-il/Desktop/Rust/ch2/ch2.1/ok_folder/target/debug/ok_folder: /Users/ohdoo-il/Desktop/Rust/ch2/ch2.1/ok_folder/ok_folder.rs

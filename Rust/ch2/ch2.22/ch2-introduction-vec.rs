d612e061565e4c089de35cea901e4b9ed4ba7299
odh <odh@kopens.com>
2022-10-16 12:25:17 +0900

    update

---
+++ Rust/ch2/ch2.22/ch2-introduction-vec.rs
@@ -0,0 +1,57 @@
+fn main(){
+    let context_lines = 2;
+    let needle = "oo";
+    let haystack = "\
+        Every face, every shop,
+        bedroom window, public-house, and
+        dark square is a picture
+        feverishly turned--in search of what?
+        It is the same with books.
+        What do we seek
+        through millions of pages?";
+
+    let mut tags: Vec<usize> = Vec::new();
+    let mut ctx:Vec<Vec<(usize, String)>> = Vec::new();
+
+    for (i, line) in haystack.lines().enumerate() {
+        if line.contains(needle) {
+            tags.push(i);
+
+            let v = Vec::with_capacity(2*context_lines + 1);
+            ctx.push(v);
+        }
+    }
+
+    if tags.is_empty() {
+        return;
+    }
+
+    println!("tags = {:?}", tags);
+    println!("ctx = {:?}", ctx);
+
+    for (i, line) in haystack.lines().enumerate() {
+        for (j, tag) in tags.iter().enumerate() {
+            let lower_bound = tag.saturating_sub(context_lines);
+            let upper_bound = tag + context_lines;
+
+            println!("line index = {}, tag index = {}", i, j);
+
+            if (i >= lower_bound) && (i <= upper_bound) {
+                print!("tag = {}, lower_bound = {}", tag, lower_bound);
+                println!(", upper_bound = {}", upper_bound);                
+                let line_as_string = String::from(line);
+                let local_ctx = (i, line_as_string);
+                ctx[j].push(local_ctx);
+            }
+        }
+    }
+
+    println!("ctx = {:?}", ctx);
+
+    for local_ctx in ctx.iter() {
+        for &(i, ref line) in local_ctx.iter() {
+            let line_num = i + 1;
+            println!("{}: {}", line_num, line);
+        }
+    }
+}

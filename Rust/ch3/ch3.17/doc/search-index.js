336730270df344fb35da93d30b1b22c6342e42d3
odh <odh@kopens.com>
2022-10-29 18:50:00 +0900

    chapter 3

---
+++ Rust/ch3/ch3.17/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"ch3_file_doced":{"doc":"한 번에 한 단계씩 파일을 시뮬레이트한다.","t":[3,11,11,11,11,11,11,11,11,11,11,11],"n":["File","borrow","borrow_mut","fmt","from","into","len","name","new","try_from","try_into","type_id"],"q":["ch3_file_doced","","","","","","","","","","",""],"d":["아마도 파일 시스템에 있을 ’파일’을 …","","","","Returns the argument unchanged.","Calls <code>U::from(self)</code>.","파일 길이를 바이트로 반환한다.","파일 이름을 반환한다.","새 파일은 비어 있다고 가정하지만 이름은 …","","",""],"i":[0,1,1,1,1,1,1,1,1,1,1,1],"f":[0,[[]],[[]],[[1,2],3],[[]],[[]],[1,4],[1,5],[6,1],[[],7],[[],7],[[],8]],"p":[[3,"File"],[3,"Formatter"],[6,"Result"],[15,"usize"],[3,"String"],[15,"str"],[4,"Result"],[3,"TypeId"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
